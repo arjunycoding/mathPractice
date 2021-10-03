3de9b07674e299e9b91107862e28002ac7bc1c0e
Arjun Yuvaraj <arjunycoding@gmail.com>
2021-10-03 10:35:59 -0400

    making a test ir practice part(IT WORKS.  WOO HOO)

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ function generateDecimal(op){
     let operation = op
     let num1= Math.floor(Math.random() * 100) + 0
     let num2= Math.floor(Math.random() * 100) + 0
-    document.write(i.toFixed(2));
+    num1 = i.toFixed(2)
 
 }
 decimals = [
@@ -19,16 +19,16 @@ decimals = [
         answer : 314 / 100                   
     },
     {
-        problem : "1.3 x 10",
-        answer : 1.3 * 10                   
+        problem : "0.285 x 1000",
+        answer :  0.285 * 1000    
     },
     {
-        problem : "1.3 x 10",
-        answer : 1.3 * 10                   
+        problem : "146 / 1000",
+        answer : 146 / 1000                   
     },
     {
-        problem : "1.3 x 10",
-        answer : 1.3 * 10                   
+        problem : "124.5 / 10",
+        answer : 124.5 / 10                   
     },
 ]
 simpleFacts = {
--- main.js
+++ main.js
@@ -1,60 +1,55 @@
-function validateForm(numberOfQuestions){
+function validateForm(numberOfQuestions, questionArray){
     let questionsRight = 0
     let questionsWrong = []
     for(let i = 0; i < numberOfQuestions; i++){
         let id = '#question' + (i + 1)
         let answer = $(id).val()
         
-        if(questions[i][1] == answer || questions[i][1] != ""){
+        if(questionArray[i][1] == answer && questionArray[i][1] != ""){
             questionsRight++
+            console.log(questionArray[i][0] + " " +answer, questionArray[i][1])
         } else {
             questionsWrong.push(i+1)
-            console.log(questions[i][0] + " " +answer, questions[i][1])
+            console.log(questionArray[i][0] + " " +answer, questionArray[i][1])
         }
     }
     return [questionsRight, questionsWrong]
 }
-function createQuestions(numberOfQuestions){
+function createQuestions(numberOfQuestions, subject){
+    let indexs = []
     let questions = []
     for(let i = 1; i <= numberOfQuestions; i++){
         let varName = 'q' + i;
-        let maxIndex = decimals.length
+        let maxIndex = subject.length
         let index = Math.floor(Math.random() * maxIndex) + 0
-        console.log(index)
-        let question = [decimals[index].problem, decimals[index].answer]
-        // console.log(decimals[maxIndex].problem, questions)
+        indexs.push(index)
+        for(let j = 0; j < indexs.length; j++){
+
+            while(indexs[j] == index){
+                index = Math.floor(Math.random() * maxIndex) + 0
+
+            }
+        }
+        let question = [subject[index].problem, subject[index].answer]
         questions.push(question)
-        // let varName = value;
     }
     return questions
 }
-let numberOfQuestions = 5
-let questions = createQuestions(5)
-for(let i = 0; i < questions.length; i++){
-    let id = i
-    id = 'question' + (i + 1)
-    $("body")
-        .append(`<span>${questions[i][0]}</span>`)
-        .append(`<input type='number' id='${id}' name='${id}'/><br/>`)
-    }
-$("body").append("<button id='submit'>SUBMIT</button><br/>")
-for(let i = 0; i < questions.length; i++){
-    let id = i
-    id = 'grade' + (i + 1)
-    $("body").append(`<span class="none circle" id="${id}"></span>&nbsp;`)
+function createButton(id, text){
+    $("body").append(`<button id=${id}>${text}</button><br/>`)
 }
-
-$("#submit").on("click", function(){
-    let score = validateForm(5)
+function giveFeedback(numberOfQuestions, questionArray){
+    let score = validateForm(numberOfQuestions, questionArray)
     let questionsWrong = score[1]
     let scoreStr = ""
     if(questionsWrong.length == 0){
-        scoreStr = `You Got ${score[0]} / 5. You Got No Questions Wrong! YOU ROCK`
+        scoreStr = `You Got ${score[0]} / ${numberOfQuestions}. You Got No Questions Wrong! YOU ROCK`
         pop()
     }
     else if(questionsWrong.length == numberOfQuestions){
-        scoreStr = `You Got ${score[0]} / 5. It's OK. You Might Have Got All The Questions Worng Now but Keep Working Hard & You'll Get It!`
-        // pop()
+        scoreStr = `You Got ${score[0]} / ${numberOfQuestions}. It's OK. You Might Have Got All The Questions Worng Now but Keep Working Hard & You'll Get It!`
+    } else{
+        scoreStr = `You Got ${score[0]} / ${numberOfQuestions}. Keep It Up!`
     }
     $("#score").html(scoreStr)
     for(let i = 0; i < questionsWrong.length; i++){
@@ -63,5 +58,44 @@ $("#submit").on("click", function(){
         $(id).addClass("wrong")
     }
     $(".none").not(".wrong").addClass("right")
+}
+function createGradeCircles(questionArray){
+    for(let i = 0; i < questionArray.length; i++){
+        let id = i
+        id = 'grade' + (i + 1)
+        $("body").append(`<span class="none circle" id="${id}"></span>&nbsp;`)
+    }
+}
+function displayQuestions(questionArray){
+    for(let i = 0; i < questionArray.length; i++){
+        let id = i
+        id = 'question' + (i + 1)
+        $("body")
+            .append(`<span>${questionArray[i][0]}</span>`)
+            .append(`<input type='number' id='${id}' name='${id}'/><br/>`)
+    }
+}
+function mathPractice(numberOfQuestions){
+    let questions = createQuestions(numberOfQuestions, decimals)
+    displayQuestions(questions)
+    createGradeCircles(questions)
+    createButton('submit', 'SUBMIT')
+    $("#submit").on("click", function(){giveFeedback(numberOfQuestions, questions)})
+    $("#typeOfTest").hide()
+    $("#continue").hide()
 
+}
+function testOrPractice(){
+    let numberOfQuestions = 0
+    if($("input[type=radio]:checked").val() == "Test"){
+        numberOfQuestions = 10
+    } else if($("input[type=radio]:checked").val() == "Practice"){
+        numberOfQuestions = 5
+    }
+    return numberOfQuestions
+}
+createButton('continue', 'CONTINUE')
+$("#continue").on("click", function(){
+    let numberOfQuestions = testOrPractice()
+    mathPractice(numberOfQuestions)
 })
